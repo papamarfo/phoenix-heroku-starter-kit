918bd0d592710972a8f1d9e3c172cb4a4033d29b
Benjamin Manford <benjaminmanford@gmail.com>
2019-04-21 09:04:41 +0100

    Update production config

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :phoenix_heroku_starter_kit, PhoenixHerokuStarterKitWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json"
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
-config:phoenix_heroku_starter_kit, PhoenixHerokuStarterKit.Repo,
+config :phoenix_heroku_starter_kit, PhoenixHerokuStarterKit.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
