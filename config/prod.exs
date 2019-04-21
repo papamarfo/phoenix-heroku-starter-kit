b2bdac25bffcea846b3799a4aaca5cd026a514fd
Benjamin Manford <benjaminmanford@gmail.com>
2019-04-21 09:02:58 +0100

    Update production config

--- config/prod.exs
+++ config/prod.exs
@@ -11,8 +11,16 @@ use Mix.Config
 # before starting your production server.
 config :phoenix_heroku_starter_kit, PhoenixHerokuStarterKitWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "phoenix-heroku-starter-kit.herokuapp.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  secret_key_base:System.get_env("SECRET_KEY_BASE")
+
+config:phoenix_heroku_starter_kit, PhoenixHerokuStarterKit.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -68,4 +76,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which should be versioned
 # separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
